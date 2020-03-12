aa73fa2bd005554190e4a9b6ced6cfc17f903f53
ktshek <ktshek@jyut.net>
2020-03-12 16:27:27 +0800

    initial commit

---
+++ source/convert.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Yaml\Yaml;
+
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader);
+
+$data = Yaml::parseFile(__DIR__ . '/assets/characters.yaml');
+
+$characters = [];
+
+foreach ($data['characters'] as $charId => $charData) {
+    $charData['id'] = $charId;
+    $tag = $charData['tag'] ?? 'UNTAGGED';
+    $sources = [];
+
+    foreach ($charData['sources'] as $sourceId => $sourceData) {
+        if (!is_array($sourceData)) {
+            $sourceData = [$sourceData];
+        }
+
+        foreach ($sourceData as $idx => $sourceDatum) {
+            $imageFilename = (count($sourceData) > 1)
+                ? sprintf('%s_%s_%d.png', $charId, $sourceId, $idx)
+                : sprintf('%s_%s.png', $charId, $sourceId);
+
+            $imagePath = __DIR__ . '/assets/images/chars/' . $imageFilename;
+            if (!is_file($imagePath)) {
+                die($imageFilename . ' not found.');
+            }
+
+            if (is_string($sourceDatum)) {
+                $sourceDatum = ['location' => $sourceDatum];
+            }
+
+            $ext = $data['sources'][$sourceId]['extension'] ?? 'jpg';
+            $evidenceFilename =
+                is_numeric($sourceDatum['location'])
+                    ? sprintf('%04d.%s', $sourceDatum['location'], $ext)
+                    : sprintf('%s.%s', $sourceDatum['location'], $ext);
+
+            $evidencePath = 'evidences/' . $sourceId . '/' . $evidenceFilename;
+            if (!is_file(__DIR__ . '/../' . $evidencePath)) {
+                die($evidencePath . ' not found.');
+            }
+
+            $sources[] = array_merge($sourceDatum, [
+                'id' => $sourceId,
+                'idx' => $idx,
+                'imageData' => base64_encode(file_get_contents($imagePath)),
+                'evidencePath' => $evidencePath,
+                'locationText' => is_numeric($sourceDatum['location']) ? 'P.' . $sourceDatum['location'] : $sourceDatum['location'],
+            ]);
+        }
+    }
+
+    $charData['srcCount'] = count($sources);
+    $charData['srcN'] = $sources;
+    $charData['remarksHTML'] =
+        isset($charData['remarks'])
+            ? \Parsedown::instance()->parse($charData['remarks'])
+            : '';
+
+    $characters[$tag][$charId] = $charData;
+}
+
+$result = $twig->render('index.html.twig', [
+    'characters' => $characters,
+    'sources' => $data['sources'],
+]);
+
+file_put_contents(__DIR__ . '/../index.html', $result);
